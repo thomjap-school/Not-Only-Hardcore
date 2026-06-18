bee268c343b7b48a491693fff234ae569973f8df
thomjap <guerni_t@etna-alternance.net>
2026-06-18 23:29:14 +0200

    Refactor: use ConfigManager for death sound settings

--- src/main/java/com/example/deathban/DeathListener.java
+++ src/main/java/com/example/deathban/DeathListener.java
@@ -3,7 +3,6 @@ package com.example.deathban;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,16 +13,12 @@ public class DeathListener implements Listener {
 
     private final JavaPlugin plugin;
     private final PrisonFeature prisonFeature;
-    private final Sound deathSound;
-    private final float soundVolume;
-    private final float soundPitch;
+    private final ConfigManager configManager;
 
-    public DeathListener(JavaPlugin plugin, PrisonFeature prisonFeature, Sound deathSound, float soundVolume, float soundPitch) {
+    public DeathListener(JavaPlugin plugin, PrisonFeature prisonFeature, ConfigManager configManager) {
         this.plugin = plugin;
         this.prisonFeature = prisonFeature;
-        this.deathSound = deathSound;
-        this.soundVolume = soundVolume;
-        this.soundPitch = soundPitch;
+        this.configManager = configManager;
     }
 
     @EventHandler
@@ -38,7 +33,12 @@ public class DeathListener implements Listener {
         plugin.getLogger().info("[DeathBan] " + player.getName() + " est mort. Cause : " + deathMessage);
 
         for (Player online : Bukkit.getOnlinePlayers()) {
-            online.playSound(online.getLocation(), deathSound, soundVolume, soundPitch);
+            online.playSound(
+                    online.getLocation(),
+                    configManager.getDeathSound(),
+                    configManager.getDeathSoundVolume(),
+                    configManager.getDeathSoundPitch()
+            );
         }
 
         Bukkit.getScheduler().runTask(plugin, () -> {
